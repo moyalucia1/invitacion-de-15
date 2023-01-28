f4f28d4aa4210e7f9455b8afb12f8ec4d8d02be0
lucia moya <moyabelenlucia@gmail.com>
2023-01-28 00:13:34 -0300

    cambios de css

--- src/Componentes/Body.js
+++ src/Componentes/Body.js
@@ -8,12 +8,12 @@ function Body() {
 
             <h1 className="titulo roll-in-left">Te invito a mi Fiesta!</h1>
             <div className="frase">
-                <h4>Aprendi que estar con quienes quiero</h4>
+                <h4>Aprendi que estar con quienes quiero...</h4>
                 <h5>es suficiente para ser Feliz, por eso quiero compartir con vos esta noche.</h5>
             </div>
 
             <div className="contenedor">
-                <p className="nombre">Hada</p>
+                <p className="nombre slide-bottom">Hada</p>
                 < h1 className="horario"> 25/03/23 21:00 hs </h1>
             </div>
 
@@ -21,9 +21,8 @@ function Body() {
 
             <h1 className="info"> Te espero para disfrutar una noche maravillosa !!
             </h1>
-            <h2 className="direccion"> En: Circulo San Costantino de Calabro, Colombia 3645 (San Justo)</h2>
-
-            <button className="boton shake-horizontal" type="text" placeholder="confirmar asistencia">CONFIRMAR ASISTENCIA</button>
+            <h2 className="direccion"> Donde? En el Circulo San Costantino de Calabro, Colombia 3645 (San Justo)</h2>
+            <a className="boton shake-horizontal" href=" https://wa.me/1132722394?text=Confirmo%20Asistencia."> Enviar Confirmacion</a>
 
         </div>
     )
